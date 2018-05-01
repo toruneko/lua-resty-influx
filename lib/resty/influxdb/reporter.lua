ddb4d6815032e112243b3aa71b3c51a64e7183d4
jianhao.dai <jianhao.dai@fraudmetrix.cn>
2018-05-01 10:05:43 +0800

    write to influxdb

--- lib/resty/influxdb/reporter.lua
+++ lib/resty/influxdb/reporter.lua
@@ -1,7 +1,10 @@
 -- Copyright (C) by Jianhao Dai (Toruneko)
 local point = require "resty.influxdb.point"
+local http = require "resty.http"
 
 local setmetatable = setmetatable
+local escape_uri = ngx.escape_uri
+local concat = table.concat
 local pairs = pairs
 
 local DEFAULT_RETENTION_POLICY = "default"
@@ -9,11 +12,18 @@ local DEFAULT_RETENTION_POLICY = "default"
 local _M = { _VERSION = '0.01' }
 local mt = { __index = _M }
 
-function _M.new(database, tags)
-    return setmetatable({
+function _M.new(url, username, password, database, tags)
+    local reporter = setmetatable({
+        url = url,
+        username = username,
+        password = password,
         database = database,
         tags = tags
     }, mt)
+
+    reporter:query(database, "CREATE DATABASE \"" + database + "\" WITH DURATION 2w REPLICATION 1 NAME \"default\"")
+
+    return reporter
 end
 
 function _M.report(self, measurement)
@@ -42,8 +52,41 @@ function _M.report(self, measurement)
     end
 end
 
-function _M.write(self, database, policy, point)
+function _M.write(self, db, rp, point)
+    local httpc = http:new()
+    httpc:set_timeout(5000)
 
+    local response = httpc:request_uri(self.url, {
+        path = "write",
+        method = "POST",
+        headers = {
+            ["Content-Type"] = "text/plain"
+        },
+        query = {
+            u = self.username,
+            p = self.password,
+            db = db,
+            rp = rp,
+            precision = "n",
+            consistency = "one"
+        },
+        body = point:lineProtocol()
+    })
+end
+
+function _M.query(self, db, q)
+    local httpc = http.new()
+    httpc:set_timeout(5000)
+    local response = httpc:request_uri(self.url, {
+        path = "query",
+        method = "POST",
+        query = {
+            u = self.username,
+            p = self.password,
+            db = db,
+            q = q
+        }
+    })
 end
 
 return _M
